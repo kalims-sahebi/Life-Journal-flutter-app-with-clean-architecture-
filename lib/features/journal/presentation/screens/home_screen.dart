8923d863542a675d88dc09fc696fbdaf593ab3dd
kalim-sahebi <kalimapid@yahoo.com>
2025-10-05 00:01:37 +0430

    state mgt with riverpod

--- lib/features/journal/presentation/screens/home_screen.dart
+++ lib/features/journal/presentation/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../state/theme_provider.dart';
 import '../state/journal_provider.dart';
 
